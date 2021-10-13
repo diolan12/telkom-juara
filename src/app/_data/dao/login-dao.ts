390a8a78bd4d524ed37ead8da29f46e5843fbabd
diolan12 <dio_lantief21@outlook.com>
2021-10-13 10:00:20 +0700

    Update login-dao.ts

--- src/app/_data/dao/login-dao.ts
+++ src/app/_data/dao/login-dao.ts
@@ -1,3 +1,12 @@
+import { Validators } from "@angular/forms";
+
+export class LoginDao {
+    public static validator = {
+        // delete in production 16982818
+        nik: ['16982818', Validators.required],
+        password: ['', Validators.required]
+    }
+}
 export interface LoginDao {
     nik: string;
     password: string;
