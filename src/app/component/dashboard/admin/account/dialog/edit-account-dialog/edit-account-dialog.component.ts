97da0db63b0262afa512eb6475f32c545eb8b8dc
diolan12 <dio_lantief21@outlook.com>
2021-10-13 10:01:05 +0700

    Update edit-account-dialog.component.ts

--- src/app/component/dashboard/admin/account/dialog/edit-account-dialog/edit-account-dialog.component.ts
+++ src/app/component/dashboard/admin/account/dialog/edit-account-dialog/edit-account-dialog.component.ts
@@ -1,15 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ToastrService } from 'ngx-toastr';
+import { AccountPutDao, PasswordErrorStateMatcher } from 'src/app/_data/dao/account-dao';
+import { Account } from 'src/app/_data/model/account';
+import { Role } from 'src/app/_data/model/role';
+import { AccountService } from 'src/app/_data/repository/account/account.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-edit-account-dialog',
   templateUrl: './edit-account-dialog.component.html',
   styleUrls: ['./edit-account-dialog.component.css']
 })
-export class EditAccountDialogComponent implements OnInit {
+export class EditAccountDialogComponent {
 
-  constructor() { }
+  togglePassword = true;
+  editAccountGroup: FormGroup;
+  roles: Array<Role> = [
+    { value: 0, text: 'Administrator' },
+    { value: 1, text: 'Kantor' },
+    { value: 2, text: 'Petugas Lapangan' }
+  ];
+  matcher = new PasswordErrorStateMatcher();
 
-  ngOnInit(): void {
+  constructor(
+    private accountService: AccountService,
+    private toastr: ToastrService,
+    public dialogRef: MatDialogRef<EditAccountDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Account,
+    private formBuilder: FormBuilder
+  ) {
+    this.editAccountGroup = this.formBuilder.group(AccountPutDao.validator(data), { validators: AccountPutDao.passwordMatcher })
+  }
+  getProfile(): string {
+    return environment.apiUrl + '/assets/profile/' + this.data.photo
+  }
+  
+  validate() {
+
+    if (this.editAccountGroup.invalid) {
+      this.toastr.error('Input tidak valid', 'Error memperbarui akun');
+    } else {
+      this.accountService.update(this.data.id, this.editAccountGroup.value).then(response => {
+        this.toastr.success('Akun ' + response.name + ' berhasil diperbarui', 'Akun diperbarui');
+        this.dialogRef.close(response)
+      }).catch(error => {
+        this.toastr.error(error.message, 'Error memperbarui akun');
+      })
+    }
+  }
+  onNoClick(): void {
+    this.dialogRef.close()
   }
 
+}
+interface Generic {
+  typesafeProp1?: number,
+    // requiredProp1: string,
+    [key: string]: any
 }
