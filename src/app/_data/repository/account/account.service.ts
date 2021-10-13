4452785f945e3750af2aae9b4f61cb466dd63121
diolan12 <dio_lantief21@outlook.com>
2021-10-13 10:00:32 +0700

    Update account.service.ts

--- src/app/_data/repository/account/account.service.ts
+++ src/app/_data/repository/account/account.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Account } from 'src/app/_data/model/account';
-import { AccountPostDao } from '../../dao/account-dao';
+import { AccountPostDao, AccountPutDao } from '../../dao/account-dao';
 
 @Injectable({
   providedIn: 'root'
@@ -10,30 +10,40 @@ import { AccountPostDao } from '../../dao/account-dao';
 export class AccountService {
 
   constructor(
-    private http: HttpClient  ) { }
+    private http: HttpClient) { }
 
   get(id: number | null = null): Promise<Array<Account>> {
     return new Promise<Array<Account>>((resolve, reject) => {
       let url = ''
       if (id === null) {
-        url = environment.apiUrl+'/api/account'
+        url = environment.apiUrl + '/api/account'
       } else {
-        url = environment.apiUrl+'/api/account/'+id
+        url = environment.apiUrl + '/api/account/' + id
       }
       this.http.get<Array<Account>>(url).toPromise()
-      .then(response => {
-        resolve(response)
-      })
-      .catch(err => { reject(err) })
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
     })
   }
+
   new(accountPostDao: AccountPostDao): Promise<Account> {
     return new Promise<Account>((resolve, reject) => {
-      this.http.post<Account>(environment.apiUrl+'/api/account', accountPostDao).toPromise()
-      .then(response => {
-        resolve(response)
-      })
-      .catch(err => { reject(err) })
+      this.http.post<Account>(environment.apiUrl + '/api/account', accountPostDao).toPromise()
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
+    })
+  }
+  update(id: number, accountPutDao: AccountPutDao): Promise<Account> {
+    return new Promise<Account>((resolve, reject) => {
+      this.http.put<Account>(environment.apiUrl + '/api/account/' + id, accountPutDao).toPromise()
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
     })
   }
 }
