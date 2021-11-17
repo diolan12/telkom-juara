17d25735c1585197d39e9fbebaecc5e0445aed43
diolan12 <dio_lantief21@outlook.com>
2021-11-17 18:43:51 +0700

    Update customer.service.ts

--- src/app/_data/repository/customer/customer.service.ts
+++ src/app/_data/repository/customer/customer.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Customer } from '../../model/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  get(id: number | null = null): Promise<Array<Customer>> {
+    return new Promise<Array<Customer>>((resolve, reject) => {
+      let url = ''
+      if (id === null) {
+        url = environment.apiUrl+'/api/customer'
+      } else {
+        url = environment.apiUrl+'/api/customer/' + id
+      }
+      this.http.get<Array<Customer>>(url).toPromise()
+      .then(response => {
+        resolve(response)
+      })
+      .catch(err => { reject(err) })
+    })
+  }
 }
