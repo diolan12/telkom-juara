2f185892326135532c5c9a62ef8b9c099fa6eb9f
diolan12 <dio_lantief21@outlook.com>
2021-11-17 07:33:28 +0700

    Create shared-home.component.ts

---
+++ src/app/component/_shared/shared-home/shared-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-shared-home',
+  templateUrl: './shared-home.component.html',
+  styleUrls: ['./shared-home.component.css']
+})
+export class SharedHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
