9669be01ac4f86008b410f9c6f26f78eb8e5982d
diolan12 <dio_lantief21@outlook.com>
2021-11-17 18:44:01 +0700

    Create customer.ts

---
+++ src/app/_data/model/customer.ts
@@ -0,0 +1,11 @@
+export interface Customer {
+    id: number
+    no_indihome: string
+    no_telephone: string | null
+    email: string
+    name: string
+    gender: string
+    phone: string
+    whatsapp: string
+    address: string
+}
