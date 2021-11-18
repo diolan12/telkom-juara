b41d5375f4c893b212fb9f714be9821f52810c37
diolan12 <dio_lantief21@outlook.com>
2021-11-19 04:54:06 +0700

    Update account.ts

--- src/app/_data/model/account.ts
+++ src/app/_data/model/account.ts
@@ -1,3 +1,4 @@
+// account model
 export interface Account {
     id: number
     nik: string
@@ -9,32 +10,3 @@ export interface Account {
     photo: string
     role: number
 }
-
-// export interface ProcessedAccount {
-//     id: number
-//     nik: string;
-//     name: string;
-//     phone: string;
-//     whatsapp: string;
-//     role: string
-// }
-
-// export function processAccount(account: Account): ProcessedAccount {
-//     let role = ''
-//     switch (account.role) {
-//         case 0: role = 'Administrator'
-//             break
-//         case 1: role = 'Petugas Kantor'
-//             break
-//         case 2: role = 'Petugas Lapangan'
-//             break
-//     }
-//     return {
-//         id: account.id,
-//         nik: account.nik,
-//         name: account.name,
-//         role: role,
-//         phone: account.phone,
-//         whatsapp: account.whatsapp
-//     }
-// }
