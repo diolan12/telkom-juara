a2f6d24ff4e5487618b4e9bea64a97f0fc662a5c
diolan12 <dio_lantief21@outlook.com>
2021-11-19 18:20:48 +0700

    Update customer-dao.ts

--- src/app/_data/dao/customer-dao.ts
+++ src/app/_data/dao/customer-dao.ts
@@ -2,52 +2,40 @@ import { Validators } from "@angular/forms";
 import { Customer } from "../model/customer";
 
 // class CustomerPostDao with a static validator
-export class CustomerPostDao {
+export class CustomerDao {
     // static customer validator
-    public static validator = {
+    public static postValidator = {
+        no_indihome: ['', Validators.maxLength(16)],
+        no_telephone: ['', Validators.maxLength(16)],
         email: ['', Validators.email],
-        name: ['', Validators.required],
+        name: ['', Validators.required, Validators.maxLength(32)],
         gender: ['', Validators.required],
-        phone: ['', Validators.required],
-        whatsapp: ['', Validators.required],
-        address: ['', Validators.required],
+        phone: ['', Validators.required, , Validators.maxLength(16)],
+        whatsapp: ['', Validators.maxLength(16)],
+        address: ['', Validators.required, Validators.maxLength(256)],
     }
-}
-
-// interface for customer method POST DAO
-export interface CustomerPostDao {
-    id: number
-    no_indihome: string
-    no_telephone: string | null
-    email: string
-    name: string
-    gender: string
-    phone: string
-    whatsapp: string
-    address: string
-}
-
-export class CustomerPutDao {
-    public static validator(data: Customer) {
+    public static putValidator(data: Customer) {
         return {
-            email: [data.email, Validators.email],
-            name: [data.name, Validators.required],
-            gender: [data.gender, Validators.required],
-            phone: [data.phone, Validators.required],
-            whatsapp: [data.whatsapp, Validators.required],
-            address: [data.address, Validators.required],
+            no_indihome: [data.no_indihome, [Validators.maxLength(16)]],
+            no_telephone: [data.no_telephone, [Validators.maxLength(16)]],
+            email: [data.email, [Validators.email]],
+            name: [data.name, [Validators.required, Validators.maxLength(32)]],
+            gender: [data.gender, [Validators.required]],
+            phone: [data.phone, [Validators.required, , Validators.maxLength(16)]],
+            whatsapp: [data.whatsapp, [Validators.maxLength(16)]],
+            address: [data.address, [Validators.required, Validators.maxLength(256)]]
         }
     }
 }
 
-// interface for customer method PUT DAO
-export interface CustomerPutDao {
-    no_indihome: string
+// interface for customer method POST DAO
+export interface CustomerDao {
+    no_indihome: string | null
     no_telephone: string | null
     email: string
     name: string
     gender: string
     phone: string
-    whatsapp: string
+    whatsapp: string | null
     address: string
 }
