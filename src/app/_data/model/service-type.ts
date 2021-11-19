21c328b98402f229c266a3068ad877af95e7bc9c
diolan12 <dio_lantief21@outlook.com>
2021-11-20 02:27:48 +0700

    Update service-type.ts

--- src/app/_data/model/service-type.ts
+++ src/app/_data/model/service-type.ts
@@ -1,4 +1,21 @@
+import { Validators } from "@angular/forms";
+
 export interface ServiceType {
     id: number;
+    name: string;
+}
+
+export class ServiceTypeDao {
+    public static postValidator = {
+        name: ['', [Validators.required, Validators.maxLength(16)]]
+    }
+    public static putValidator(data: ServiceType) {
+        return {
+            name: [data.name, [Validators.required, Validators.maxLength(16)]]
+        }
+    }
+}
+
+export interface ServiceTypeDao {
     name: string;
 }
