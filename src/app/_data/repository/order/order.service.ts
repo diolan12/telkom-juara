243a13e80b0e3ce5f06cd70fb24bdc7fa64b9c93
diolan12 <dio_lantief21@outlook.com>
2021-11-21 06:27:38 +0700

    Update order.service.ts

--- src/app/_data/repository/order/order.service.ts
+++ src/app/_data/repository/order/order.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/example.environment';
-import { Order } from '../../model/order';
+import { Order, OrderDTO } from '../../model/order';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +13,7 @@ export class OrderService {
 
   // function to get all orders return type promise of order array
   // require parameters of nullable id and string status
-  get(id: number |null, status: string| null): Promise<Array<Order>> {
+  get(id: number |null = null, status: string| null = null): Promise<Array<Order>> {
     return new Promise<Array<Order>>((resolve, reject) => {
       // blank url then decide whether id is null or not
       // if id is null then get all account
@@ -29,6 +29,26 @@ export class OrderService {
         url = environment.apiUrl + '/api/order/'+id;
       }
       this.http.get<Array<Order>>(url).toPromise()
+      .then(response => {
+        resolve(response)
+      })
+      .catch(err => { reject(err) })
+    })
+  }
+
+  create(orderDto: OrderDTO): Promise<Order> {
+    return new Promise<Order>((resolve, reject) => {
+      this.http.post<Order>(environment.apiUrl + '/api/order', orderDto).toPromise()
+      .then(response => {
+        resolve(response)
+      })
+      .catch(err => { reject(err) })
+    })
+  }
+
+  update(id: number, orderDto: OrderDTO): Promise<Order> {
+    return new Promise<Order>((resolve, reject) => {
+      this.http.put<Order>(environment.apiUrl + '/api/order/'+id, orderDto).toPromise()
       .then(response => {
         resolve(response)
       })
