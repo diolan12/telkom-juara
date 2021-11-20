aa3661e5ae9d9e8cdc9d4967138a55136c1e5659
diolan12 <dio_lantief21@outlook.com>
2021-11-20 08:49:56 +0700

    Update order.component.ts

--- src/app/component/_shared/operation/order/order.component.ts
+++ src/app/component/_shared/operation/order/order.component.ts
@@ -1,7 +1,9 @@
 import { AfterViewInit, ViewChild } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Order } from 'src/app/_data/model/order';
 
 export interface PeriodicElement {
   name: string;
@@ -10,30 +12,35 @@ export interface PeriodicElement {
   symbol: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
 
-export class OrderComponent implements OnInit, AfterViewInit  {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class OrderComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  status: string | null = null;
+  orderControl: FormGroup;
+  panelOpenState = [false, false, false];
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = new MatTableDataSource<Order>([{
+    id: 1,
+    uid: 'SC-2021102000001',
+    field: null,
+    office: 1,
+    status: 'ongoing',
+    customer: 1,
+    service_type: 1
+  }]);
+
+  constructor(
+    private formBuilder: FormBuilder) {
+    this.orderControl = this.formBuilder.group({
+      options: ['null']
+    })
+  }
   // @ViewChild(MatSort) sort: MatSort;
   ngAfterViewInit(): void {
     // this.dataSource.sort = this.sort;
