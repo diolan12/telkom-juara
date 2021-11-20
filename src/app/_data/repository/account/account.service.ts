86282b01e412870ff1f846cc6491ab71797abc05
diolan12 <dio_lantief21@outlook.com>
2021-11-21 06:27:31 +0700

    Update account.service.ts

--- src/app/_data/repository/account/account.service.ts
+++ src/app/_data/repository/account/account.service.ts
@@ -83,7 +83,7 @@ export class AccountService {
   getRoleValue(role: number): string {
     switch (role) {
       case 0: return 'Administrator';
-      case 1: return 'Petugas Kantor';
+      case 1: return 'Petugas Kantosr';
       case 2: return 'Petugas Lapangan';
     }
     return ''
