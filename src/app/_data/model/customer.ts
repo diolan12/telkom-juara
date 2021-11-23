0b2ac06d091e71c3374577df829077d91a75f078
diolan12 <dio_lantief21@outlook.com>
2021-11-24 05:28:28 +0700

    Update customer.ts

--- src/app/_data/model/customer.ts
+++ src/app/_data/model/customer.ts
@@ -1,3 +1,4 @@
+import { Validators } from "@angular/forms";
 // customer model
 export interface Customer {
     id: number
@@ -8,5 +9,43 @@ export interface Customer {
     gender: string
     phone: string
     whatsapp: string
+    address: string
+}
+
+export class CustomerDto {
+    // static customer validator
+    public static postValidator = {
+        no_indihome: ['', [Validators.maxLength(16)]],
+        no_telephone: ['', [Validators.maxLength(16)]],
+        email: ['', [Validators.email]],
+        name: ['', [Validators.required, Validators.maxLength(32)]],
+        gender: ['', [Validators.required]],
+        phone: ['', [Validators.required, Validators.maxLength(16)]],
+        whatsapp: ['', [Validators.maxLength(16)]],
+        address: ['', [Validators.required, Validators.maxLength(256)]],
+    }
+    public static putValidator(data: Customer) {
+        return {
+            no_indihome: [data.no_indihome, [Validators.maxLength(16)]],
+            no_telephone: [data.no_telephone, [Validators.maxLength(16)]],
+            email: [data.email, [Validators.email]],
+            name: [data.name, [Validators.required, Validators.maxLength(32)]],
+            gender: [data.gender, [Validators.required]],
+            phone: [data.phone, [Validators.required, Validators.maxLength(16)]],
+            whatsapp: [data.whatsapp, [Validators.maxLength(16)]],
+            address: [data.address, [Validators.required, Validators.maxLength(256)]]
+        }
+    }
+}
+
+// interface for customer method POST DAO
+export interface CustomerDto {
+    no_indihome: string | null
+    no_telephone: string | null
+    email: string
+    name: string
+    gender: string
+    phone: string
+    whatsapp: string | null
     address: string
 }
