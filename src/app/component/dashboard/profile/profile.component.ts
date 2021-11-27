a8577082e84e234e6aab66dca875b6b97279ab47
diolan12 <dio_lantief21@outlook.com>
2021-11-27 13:19:10 +0700

    Update profile.component.ts

--- src/app/component/dashboard/profile/profile.component.ts
+++ src/app/component/dashboard/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Account } from 'src/app/_data/model/account';
+import { AuthService } from 'src/app/_data/service/auth.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  
+  background = environment.apiUrl+'/assets/image/background.jpg'
 
-  constructor() { }
+  account: Account | null = null;
+
+  constructor(
+    private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.authService.account().then((account) => {
+      this.account = account
+    }).catch((err) => {
+      console.error(err)
+    })
   }
 
 }
