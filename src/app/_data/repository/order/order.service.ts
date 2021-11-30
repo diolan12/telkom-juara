a7ffb97bbd0f38d7f38f61f55178f13c5d86b1ba
diolan12 <dio_lantief21@outlook.com>
2021-11-30 13:36:27 +0700

    Update order.service.ts

--- src/app/_data/repository/order/order.service.ts
+++ src/app/_data/repository/order/order.service.ts
@@ -13,46 +13,51 @@ export class OrderService {
 
   // function to get all orders return type promise of order array
   // require parameters of nullable id and string status
-  get(id: number |null = null, status: string| null = null): Promise<Array<Order>> {
-    return new Promise<Array<Order>>((resolve, reject) => {
+  get<T>(uid: string | null = null, status: string | null = null): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
       // blank url then decide whether id is null or not
       // if id is null then get all account
       // else get account by id
       let url = '';
-      if (id === null) {
+      if (uid === null) {
         if (status !== null) {
-          url = environment.apiUrl + '/api/order?relation&clean&where=status-is-'+status;
+          url = environment.apiUrl + '/api/order?relation&clean&where=status.is.' + status;
         } else {
           url = environment.apiUrl + '/api/order?relation&clean';
         }
       } else {
-        url = environment.apiUrl + '/api/order/'+id+'?relation';
+        url = environment.apiUrl + '/api/order?where=uid.is.' + uid + '&relation';
+        this.http.get<T>(url).toPromise()
+          .then(response => {
+            resolve(response)
+          })
+          .catch(err => { reject(err) })
       }
-      this.http.get<Array<Order>>(url).toPromise()
-      .then(response => {
-        resolve(response)
-      })
-      .catch(err => { reject(err) })
+      this.http.get<T>(url).toPromise()
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
     })
   }
 
   create(orderDto: OrderDTO): Promise<Order> {
     return new Promise<Order>((resolve, reject) => {
       this.http.post<Order>(environment.apiUrl + '/api/order', orderDto).toPromise()
-      .then(response => {
-        resolve(response)
-      })
-      .catch(err => { reject(err) })
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
     })
   }
 
   update(id: number, orderDto: OrderDTO): Promise<Order> {
     return new Promise<Order>((resolve, reject) => {
-      this.http.put<Order>(environment.apiUrl + '/api/order/'+id, orderDto).toPromise()
-      .then(response => {
-        resolve(response)
-      })
-      .catch(err => { reject(err) })
+      this.http.put<Order>(environment.apiUrl + '/api/order/' + id, orderDto).toPromise()
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
     })
   }
 
