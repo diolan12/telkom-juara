e6abf6035434c527c31a5f0c8f656c32f1101acd
diolan12 <dio_lantief21@outlook.com>
2021-12-01 12:19:42 +0700

    Update customer.ts

--- src/app/_data/model/customer.ts
+++ src/app/_data/model/customer.ts
@@ -2,26 +2,26 @@ import { Validators } from "@angular/forms";
 // customer model
 export interface Customer {
     id: number
-    no_indihome: string
+    no_indihome: string | null
     no_telephone: string | null
     email: string
     name: string
     gender: string
     phone: string
-    whatsapp: string
+    whatsapp: string | null
     address: string
 }
 
 export class CustomerDto {
     // static customer validator
     public static postValidator = {
-        no_indihome: ['', [Validators.maxLength(16)]],
-        no_telephone: ['', [Validators.maxLength(16)]],
+        no_indihome: [null, [Validators.maxLength(16)]],
+        no_telephone: [null, [Validators.maxLength(16)]],
         email: ['', [Validators.email]],
         name: ['', [Validators.required, Validators.maxLength(32)]],
         gender: ['', [Validators.required]],
         phone: ['', [Validators.required, Validators.maxLength(16)]],
-        whatsapp: ['', [Validators.maxLength(16)]],
+        whatsapp: [null, [Validators.maxLength(16)]],
         address: ['', [Validators.required, Validators.maxLength(256)]],
     }
     public static putValidator(data: Customer) {
