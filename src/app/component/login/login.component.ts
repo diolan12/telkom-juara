f99e1a460b46f0251fcf583b25553b5b49e3f4a3
diolan12 <dio_lantief21@outlook.com>
2021-12-13 23:54:02 +0700

    Update login.component.ts

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -3,7 +3,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 
-import { LoginDTO } from 'src/app/_data/dao/login-dto';
+import { LoginDTO } from 'src/app/_data/dto/login-dto';
 import { AuthService } from 'src/app/_data/service/auth.service'
 
 @Component({
