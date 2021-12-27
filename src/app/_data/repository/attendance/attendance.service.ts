6279be160d9dbc94a260330d113c9b6a34c28376
diolan12 <dio_lantief21@outlook.com>
2021-12-28 05:38:55 +0700

    Update attendance.service.ts

--- src/app/_data/repository/attendance/attendance.service.ts
+++ src/app/_data/repository/attendance/attendance.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Attendance, AttendanceDTO } from '../../model/attendance';
 
 @Injectable({
   providedIn: 'root'
@@ -8,4 +10,30 @@ export class AttendanceService {
 
   constructor(
     private http: HttpClient) { }
+
+  get(account: number | null = null): Promise<Array<Attendance>> {
+    let url = ''
+    if (account === null) {
+      url = environment.apiUrl + '/api/attendance?relation'
+    } else {
+      url = environment.apiUrl + '/api/attendance?relation&where=account.is.' + account
+    }
+    return new Promise<Array<Attendance>>((resolve, reject) => {
+      this.http.get<Array<Attendance>>(url).toPromise()
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
+    })
+  }
+
+  create(attendanceDTO: AttendanceDTO): Promise<Attendance> {
+    return new Promise<Attendance>((resolve, reject) => {
+      this.http.post<Attendance>(environment.apiUrl + '/api/attendance', attendanceDTO).toPromise()
+        .then(response => {
+          resolve(response)
+        })
+        .catch(err => { reject(err) })
+    })
+  }
 }
