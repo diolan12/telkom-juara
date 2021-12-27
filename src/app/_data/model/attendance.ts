991f5b4d9e9f683d513e78cb96876151129b2673
diolan12 <dio_lantief21@outlook.com>
2021-12-28 05:38:49 +0700

    Update attendance.ts

--- src/app/_data/model/attendance.ts
+++ src/app/_data/model/attendance.ts
@@ -1,8 +1,18 @@
+import { Validators } from "@angular/forms";
 import { Account } from "./account";
 
 export interface Attendance {
     id: number;
     account: Account;
-    created_at: Date;
-    updated_at: Date;
+    created_at: string;
+}
+
+export class AttendanceDTO {
+    public static postValidator = {
+        account: [0, [Validators.required]],
+    }
+}
+
+export interface AttendanceDTO {
+    account: number;
 }
