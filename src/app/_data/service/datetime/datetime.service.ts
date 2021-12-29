d52599dc42587dd16bcaa361d58603a645f34c2b
diolan12 <dio_lantief21@outlook.com>
2021-12-30 00:24:11 +0700

    Update datetime.service.ts

--- src/app/_data/service/datetime/datetime.service.ts
+++ src/app/_data/service/datetime/datetime.service.ts
@@ -6,17 +6,30 @@ import { Injectable } from '@angular/core';
 export class DatetimeService {
 
   private dateTime: Date
-  constructor() { 
+  constructor() {
     this.dateTime = new Date();
   }
   UTCtoLocal(date: Date) {
-    var newDate = new Date(date.getTime()+date.getTimezoneOffset()*60*1000);
+    var newDate = new Date(date.getTime() + date.getTimezoneOffset() * 60 * 1000);
 
     var offset = this.dateTime.getTimezoneOffset() / 60;
     var hours = date.getHours();
 
     newDate.setHours(hours - offset);
 
-    return newDate;   
-}
+    return newDate;
+  }
+
+  getDayName(dayOfWeek: number): string {
+    switch (dayOfWeek) {
+      case 0: return 'Minggu';
+      case 1: return 'Senin';
+      case 2: return 'Selasa';
+      case 3: return 'Rabu';
+      case 4: return 'Kamis';
+      case 5: return 'Jumat';
+      case 6: return 'Sabtu';
+    }
+    return '';
+  }
 }
