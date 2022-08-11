1ed331f217cc661681f8b07c5facb7ee7fd7ed7e
diolan12 <dio_lantief21@outlook.com>
2022-08-11 22:13:33 +0700

    Update profile.component.ts

--- src/app/component/dashboard/profile/profile.component.ts
+++ src/app/component/dashboard/profile/profile.component.ts
@@ -13,6 +13,7 @@ export class ProfileComponent implements OnInit {
   background = environment.apiUrl+'/assets/image/background.jpg'
 
   account: Account | null = null;
+  role: string = ""
 
   constructor(
     private authService: AuthService) { }
@@ -20,6 +21,18 @@ export class ProfileComponent implements OnInit {
   ngOnInit(): void {
     this.authService.account().then((account) => {
       this.account = account
+      switch (account.role) {
+        case 0:
+          this.role = "Administrator"
+          break;
+        case 1:
+          this.role = "Petugas Kantor"
+          break;
+        case 2:
+          this.role = "Petugas Lapangan"
+          break;
+      }
+
     }).catch((err) => {
       console.error(err)
     })
